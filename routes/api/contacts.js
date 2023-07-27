68e5f39a459ac7746b93d0cf560503789adad2f5
VladyslavLata <vladlatahtc@gmail.com>
2023-07-27 17:37:53 +0300

    fix: on 888

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  res.json({ message: "template message 777" });
+  res.json({ message: "template message 888" });
 });
 
 router.get("/:contactId", async (req, res, next) => {
