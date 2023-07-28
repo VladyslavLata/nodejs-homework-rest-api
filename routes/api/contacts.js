6d7640e956bd27f93b6d329e84ac4a024d678cb5
VladyslavLata <vladlatahtc@gmail.com>
2023-07-28 17:53:24 +0300

    s

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,19 +3,19 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  res.json({ message: "template message 2" });
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  res.json({ message: "template message 2" });
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  res.json({ message: "template message 2" });
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  res.json({ message: "template message 2" });
 });
 
 router.put("/:contactId", async (req, res, next) => {
