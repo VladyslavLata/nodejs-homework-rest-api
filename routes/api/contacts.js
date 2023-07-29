413a5233388588004eba6d692912c12caaa34e22
VladyslavLata <vladlatahtc@gmail.com>
2023-07-29 16:49:09 +0300

    s

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -7,15 +7,15 @@ router.get("/", async (req, res, next) => {
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message 2" });
+  res.json({ message: "template message 23" });
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "template message 2" });
+  res.json({ message: "template message 23" });
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message 2" });
+  res.json({ message: "template message 23" });
 });
 
 router.put("/:contactId", async (req, res, next) => {
